f28b07041337bd760d4e91487f620c88ce81e857
Stepan <lavash@tut.by>
2021-03-15 22:27:20 +0300

    feat: write code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
-}
+    let result = '';
+    let m=Math.abs(n);
+    while(m>0){
+    result = result + (m%10);
+    m = parseInt(m/10);
+    }
+    return result;
+  }
